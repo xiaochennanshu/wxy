60f90c96c41973829330c2e5ab3fd806522e8550
小宇sir <2794476065@qq.com>
2023-06-10 22:29:01 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/wxy/",
+  build: {
+    outDir: "docs"
+  },
   plugins: [
     vue(), 
     vueJsx(),
